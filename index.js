30761d3e852a926f0f9ded74c62abb6daea6dcaa
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-09-29 20:19:04 +0200

    Changed some text...

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             var text = event.message.text;
 
-            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            sendTextMessage(sender, "U w0t m8, echo: "+ text.substring(0, 200));
         }
     }
 
