f3b9e66117a5b39ba9b9151a8c8d55298c508c96
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-09-30 23:08:18 +0200

    Changed dir

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const api = require('api.js')
+const api = require('./api.js')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
