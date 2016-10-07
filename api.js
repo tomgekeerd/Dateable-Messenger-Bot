70286e2919a54d752bb7638cb223003a6f45362c
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-08 00:00:13 +0200

    API

--- api.js
+++ api.js
@@ -249,6 +249,25 @@ var self = module.exports = {
 
         self.sendGreetingMessages(webhook.recipient_id, firstname)
 
+        const results = [];
+
+        pg.defaults.ssl = true;
+        pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+            if(err) {
+                done();
+                console.log(err);
+            }
+
+            const query = client.query(`INSERT INTO users (last_name, first_name, gender, looking_for, profile_pic, fb_id) VALUES ('${lastname}', '${firstname}', ${gender}, -1, '${profile_pic}', ${webhook.recipient_id});`);
+            query.on('row', (row) => {
+                results.push(row);
+            });
+
+            query.on('end', () => {
+                done();
+            });
+        });
+
     }
 
 }
