90d26fbbeb742e4a73764b56effa886d3c51d2b5
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-08 00:55:53 +0200

    I'm gonna win this

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/webhook/', function (req, res) {
 
                 case "showPrivacySettings":
 
-                    if (payload.data == true) {
+                    if (postback.data == true) {
                         // TODO
                     } else {
 
