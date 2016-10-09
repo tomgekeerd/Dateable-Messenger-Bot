0d0504fbe477d61b7d389572a8faf2b134069c89
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-09 18:49:34 +0200

    you were out shoudl

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.post('/webhook/', function (req, res) {
                     done();
                 }
 
-                client.query(`UPDATE users SET loc_latitude=${api.loc_latitude}, loc_longitude=${api.loc_longitude}, geo_location=${} WHERE fb_id=${recipient_id};`);
+                client.query(`UPDATE users SET loc_latitude=${api.loc_latitude}, loc_longitude=${api.loc_longitude}, geo_location=${api.geo_location} WHERE fb_id=${recipient_id};`);
 
                 const query = client.query(`SELECT * FROM users;`);
                 query.on('row', function(row) {
