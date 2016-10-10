faaf63b615f2eff5e4ab4a7943542db23eaec419
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-10 09:50:57 +0200

    Kleine test

--- api.js
+++ api.js
@@ -175,6 +175,11 @@ var self = module.exports = {
             search_query.on('row', function(row) {
                 found_array.push(row);
             })
+
+            search_query.on('end', () => {
+                done();
+                return found_array;
+            })
         })
     },
 
@@ -354,6 +359,22 @@ var self = module.exports = {
 
         });
 
+    },
+
+    getDistanceFromLatLonInKm: function(lat1, lon1, lat2, lon2) {
+        var R = 6371; 
+        var dLat = deg2rad(lat2-lat1);
+        var dLon = deg2rad(lon2-lon1); 
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; 
+
+        return d;
+    },
+
+    deg2rad: function(deg) {
+        return deg * (Math.PI/180)
     }
 
 }
--- index.js
+++ index.js
@@ -178,22 +178,6 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; 
-    var dLat = deg2rad(lat2-lat1);
-    var dLon = deg2rad(lon2-lon1); 
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; 
-
-    return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
 // spin spin sugar
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
