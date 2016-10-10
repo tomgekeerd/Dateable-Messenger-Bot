d0480f2ea0af509640b502d37be3e39ed69a0ec4
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-10 11:42:37 +0200

    no errors

--- api.js
+++ api.js
@@ -400,10 +400,10 @@ var self = module.exports = {
 
     getDistanceFromLatLonInKm: function(lat1, lon1, lat2, lon2) {
         var R = 6371; 
-        var dLat = deg2rad(lat2-lat1);
-        var dLon = deg2rad(lon2-lon1); 
+        var dLat = self.deg2rad(lat2-lat1);
+        var dLon = self.deg2rad(lon2-lon1); 
 
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(self.deg2rad(lat1)) * Math.cos(self.deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         var d = R * c; 
 
