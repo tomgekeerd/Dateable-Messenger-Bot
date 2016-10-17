438666b2f792ed4a1471fb362900b17c7b2a49b0
Tom de Ruiter <tomderuiter.hhw@hotmail.com>
2016-10-17 20:41:16 +0200

    did u have to go to jail?

--- api.js
+++ api.js
@@ -381,11 +381,13 @@ var self = module.exports = {
 
                 var i = 0;
                 while (i < big_found_array.length) {
+                    console.log("valerie" + i)
                     const blocked = client.query(`SELECT COUNT(*) FROM blocked_users WHERE fb_id=${big_found_array[i].fb_id} AND blocked=${id};`)
                     blocked.on('row', function(row) {
                         if (row.count > 0) {
                             console.log(big_found_array)
-                            console.log(i)
+                            console.log("count" + i)
+                            console.log(row)
                             var removed = big_found_array.splice(i, 1);
                             console.log(removed)
                         }
